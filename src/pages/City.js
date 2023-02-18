af4f9476efbea1f9f0d2bb3fea9fbfdac1b713aa
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-02-18 09:43:05 -0800

    city

---
+++ src/pages/City.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import City from  "../images/City.jpg";
+export default function City() {
+  return (
+    <div className="App">
+        <h1>Count by City Visual</h1>
+        <img src={City} height={900} width={1300}/>
+        
+        </div>
+  )
+}
