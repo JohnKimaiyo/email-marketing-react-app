4bfebe233d7588b56c88fd37292fc7be5560d44f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-02-19 10:35:57 +0300

    count by time

---
+++ src/pages/Time.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Time from "../images/Image.jpg";
+export default function Time() {
+  return (
+    <div>
+      <h1>Count b Time visual</h1>
+      <img src={Time} height={900} width={1300} />
+    </div>
+  );
+}
