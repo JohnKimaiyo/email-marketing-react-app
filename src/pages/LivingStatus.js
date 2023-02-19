13b6ba2e84125982728bc5a1ea24c796a61cb193
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-02-19 12:31:14 +0300

    adding navlink to livingstatus page

--- src/pages/LivingStatus.js
+++ src/pages/LivingStatus.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Status from "../images/Status.jpg";
-import "./App.css"
+import "./App.css";
+import { NavLink } from "react-router-dom";
 export default function LivingStatus() {
   return (
     <div>
       <h1>Count on Living Status</h1>
+      <p>
+        Go to the <NavLink to="/">HomePage</NavLink>
+      </p>
       <img src={Status} height={900} width={1300} />
     </div>
   );
