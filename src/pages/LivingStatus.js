5af0c5aa54f68a0d1afd2c6bde1e719768ff1d9b
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-02-19 09:54:14 +0300

    status visual

---
+++ src/pages/LivingStatus.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Status from "../images/Status.jpg";
+export default function LivingStatus() {
+  return (
+    <div>
+      <h1>Count on Living Status</h1>
+      <img src={Status} height={900} width={1300} />
+    </div>
+  );
+}
