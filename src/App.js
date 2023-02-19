ac7dd04e2649b50f4206eb541b671db705391ee0
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-02-19 11:50:07 +0300

    adding navlink to city page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
+//import pages
+import HomePage from "./pages/HomePage";
+import City from "./pages/City";
+import Gender from "./pages/Gender";
+import LivingStatus from "./pages/LivingStatus";
+import MaritalStatus from "./pages/MaritalStatus";
+import State from "./pages/State";
+import Time from "./pages/Time";
+import Year from "./pages/Year";
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/pages/City.js
+++ src/pages/City.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import City from  "../images/City.jpg";
-import './App.css'
+import React from "react";
+import City from "../images/City.jpg";
+import "./App.css";
+import { NavLink } from "react-router-dom";
 export default function City() {
   return (
     <div className="App">
-        <h1>Count by City Visual</h1>
-        <img src={City} height={900} width={1300}/>
-        
-        </div>
-  )
+      <h1>Count by City Visual</h1>
+      <p>
+        Go to <NavLink to="/">HomePage</NavLink>
+      </p>
+      <img src={City} height={900} width={1300} />
+    </div>
+  );
 }
