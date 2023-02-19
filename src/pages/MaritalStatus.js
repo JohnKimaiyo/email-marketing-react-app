126910c46c1fa9ddf280cb1428440f42fa927409
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-02-19 12:34:05 +0300

    ading navlink to marital status page

--- src/pages/MaritalStatus.js
+++ src/pages/MaritalStatus.js
@@ -1,10 +1,14 @@
 import React from "react";
-import "./App.css"
+import { NavLink } from "react-router-dom";
+import "./App.css";
 import Marital from "../images/Marital.jpg";
 export default function MaritalStatus() {
   return (
     <div>
       <h1>Count on Marital status</h1>
+      <p>
+        Fo the <NavLink to="/">Home Page</NavLink>
+      </p>
       <img src={Marital} height={900} width={1300} />
     </div>
   );
