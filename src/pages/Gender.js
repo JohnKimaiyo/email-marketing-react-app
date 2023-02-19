fb0bdfb3e6032de0e9b6fd4c95e732fdacec4cf1
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-02-19 21:03:51 +0300

    adding cs

--- src/pages/Gender.js
+++ src/pages/Gender.js
@@ -2,14 +2,15 @@ import React from "react";
 
 import "../App.css";
 
-import gender from "../images/gender.jpg"
-
+import gender from "../images/gender.jpg";
 
 export default function Gender() {
   return (
     <div className="App">
       <h1>Count by Visuals</h1>
-     
+      <p>
+        Return to <NavLink to="/">HomePage</NavLink>
+      </p>
       <img src={gender} height={900} width={1300} />
     </div>
   );
--- src/pages/LivingStatus.js
+++ src/pages/LivingStatus.js
@@ -4,12 +4,12 @@ import "../App.css";
 import { NavLink } from "react-router-dom";
 export default function LivingStatus() {
   return (
-    <div  className="App.css">
+    <div className="App.css">
       <h1>Count on Living Status</h1>
       <p>
         Go to the <NavLink to="/">HomePage</NavLink>
       </p>
-      <img src={status} height={900} width={1300} />
+      <img src={status} height={900} width={1800} />
     </div>
   );
 }
