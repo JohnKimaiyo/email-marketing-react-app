9fb60abed1d0774ff1eb4fbf63e5c47bd0cc53f7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-02-19 10:38:24 +0300

    cpunt by year

---
+++ src/pages/Year.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Year from "../images/Year.jpg";
+export default function Year() {
+  return (
+    <div>
+      <h1>Count by Year</h1>
+      <img src={Year} heaight={900} width={1300} />
+    </div>
+  );
+}
